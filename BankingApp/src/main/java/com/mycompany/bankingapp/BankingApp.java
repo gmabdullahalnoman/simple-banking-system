cc0541dbcfd30135bcecf8b4d2d967324f4829d2
Noman <gmabdullahalnoman@gmail.com>
2025-08-21 19:39:08 +0600

    Initial commit

---
+++ BankingApp/src/main/java/com/mycompany/bankingapp/BankingApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bankingapp;
+
+/**
+ *
+ * @author abdullahalnoman
+ */
+public class BankingApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
